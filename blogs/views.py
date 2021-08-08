c4f3292e3916a0ce23b958c25c1fe505b0b37f06
pulosez <pulosez@gmail.com>
2021-08-08 17:52:31 +0300

    Added protection to app 'blogs'

--- blogs/views.py
+++ blogs/views.py
@@ -1,30 +1,43 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.http import Http404
 
 from .models import BlogPost
 from .forms import PostForm
 
 
+def check_post_owner(owner, user):
+    if owner != user:
+        raise Http404
+
+
+@login_required
 def posts(request):
-    posts = BlogPost.objects.order_by('date_added')
+    posts = BlogPost.objects.filter(owner=request.user).order_by('date_added')
     context = {'posts': posts}
     return render(request, 'blogs/posts.html', context)
 
 
+@login_required
 def new_post(request):
     if request.method != 'POST':
         form = PostForm()
     else:
         form = PostForm(data=request.POST)
         if form.is_valid():
-            form.save()
+            new_post = form.save(commit=False)
+            new_post.owner = request.user
+            new_post.save()
             return redirect('blogs:posts')
 
     context = {'form': form}
     return render(request, 'blogs/new_post.html', context)
 
 
+@login_required
 def edit_post(request, post_id):
     post = BlogPost.objects.get(id=post_id)
+    check_post_owner(post.owner, request.user)
 
     if request.method != 'POST':
         form = PostForm(instance=post)
